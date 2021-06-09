47c8e7fa3bd00e8105f794bb5581c877b7518e94
Moritz Weingraber <weingraber.m03@htlwienwest.at>
2021-06-09 03:56:21 +0200

    Es is 4 in der Früh. Nichts funktioniert.

---
+++ CasinooWPF/Data.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CasinooWPF
+{
+    public class Data
+    {
+        public int counter { get; set; }
+        public int bet { get; set; }
+        public int credit { get; set; }
+    }
+}
--- CasinooWPF/MainWindow.xaml.cs
+++ CasinooWPF/MainWindow.xaml.cs
@@ -13,6 +13,12 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+//using Json.Net;
+using Newtonsoft.Json;
+using System.IO;
+using System.Collections.ObjectModel;
 
 namespace CasinooWPF
 {
@@ -21,35 +27,50 @@ namespace CasinooWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
             timer1.Tick += timer1_Tick;
         }
+        Data data = new Data
+        {
+            counter = 0,
+            bet = 5,
+            credit = 50
+        };
         Random rnd = new Random();
         int m;
         int a;
         int b;
         int c;
-        int credit = 50;
-        int bet = 5;
-
+        //int credit = 50;
+        //int bet = 5;
+        //int counter = 0;
+        int button = 0;
+        int coutIntern = 0;
         DispatcherTimer timer1 = new DispatcherTimer();
-        
+        public List<Data> result = new List<Data>();
+
+        //List<Data> desData = JsonConvert.DeserializeObject<List<Data>>(@"../../../TxtFile/Data.json");
+
         private void timer1_Tick(object sender, EventArgs e)
         {
             m = m + 10;
+            //MessageBox.Show("I tic");
             if (m < 100)
             {
-
                 a = Convert.ToInt32(1 + rnd.NextDouble() * 3);
 
                 b = Convert.ToInt32(1 + rnd.NextDouble() * 3);
 
                 c = Convert.ToInt32(1 + rnd.NextDouble() * 3);
-                Uri Watermelon = new Uri("C:/Users/morit/Desktop/Schule/SEW/Casino/CasinoWPF/CasinooWPF/images/rsz_watermelon.png");
-                Uri Grapes = new Uri("C:/Users/morit/Desktop/Schule/SEW/Casino/CasinoWPF/CasinooWPF/images/rsz_grapes.png");
-                Uri Sedmica = new Uri("C:/Users/morit/Desktop/Schule/SEW/Casino/CasinoWPF/CasinooWPF/images/rsz_sedmica.bmp");
+                //Uri Watermelon = new Uri(@"..\..\..\images\rsz_watermelon.png", UriKind.Relative);
+                //Uri Grapes = new Uri(@"..\..\..\images\rsz_grapes.png", UriKind.Relative);
+                //Uri Sedmica = new Uri(@"..\..\..\images\rsz_sedmica.bmp", UriKind.Relative);
+                Uri Watermelon = new Uri(@"C:\Users\morit\Desktop\Schule\SEW\Casino\CasinoWPF\CasinooWPF\images\rsz_watermelon.png");
+                Uri Grapes = new Uri(@"C:\Users\morit\Desktop\Schule\SEW\Casino\CasinoWPF\CasinooWPF\images\rsz_grapes.png");
+                Uri Sedmica = new Uri(@"C:\Users\morit\Desktop\Schule\SEW\Casino\CasinoWPF\CasinooWPF\images\rsz_sedmica.bmp");
 
                 switch (a)
                 {
@@ -96,41 +117,78 @@ namespace CasinooWPF
             }
             else
             {
-                timer1.IsEnabled = false;
+                if (button==1&& coutIntern >= 0)
+                {
+                    coutIntern = 0;
+                    timer1.IsEnabled = false;
+                }
+                else if (button==2&& coutIntern == 100)
+                {
+                    timer1.IsEnabled = false;
+                    MessageBox.Show(Convert.ToString(data.counter));
+                    coutIntern = 0;
+                }
                 m = 0;
                 if ((a == b) && (a == c))
                 {
-                    lblMsg.Content = "Jackpot! You won " + (2 * bet).ToString() + " $!!!";
-                    credit = credit + bet;
-                    infolbl.Content = "CREDIT: " + credit.ToString() + " $";
-
+                    lblMsg.Content = "Jackpot! You won " + (2 * data.bet).ToString() + " $!!!";
+                    data.credit = data.credit + data.bet;
+                    infolbl.Content = "CREDIT: " + data.credit.ToString() + " $";
+                    //Data indiv = new Data
+                    //{
+                    //    counter = data.counter + 1,
+                    //    bet = -data.bet,
+                    //    credit = data.credit
+                    //};
+                    //result.Add(indiv);
+                    coutIntern++;
+                    data.counter += 1;
                 }
                 else
                 {
                     lblMsg.Content = "No luck, try again";
-                    credit = credit - bet;
-                    infolbl.Content = "CREDIT: " + credit.ToString() + " $";
-                    if (credit < bet)
+                    data.credit = data.credit - data.bet;
+                    infolbl.Content = "CREDIT: " + data.credit.ToString() + " $";
+                    if (data.credit < data.bet)
                     {
                         Button1.IsEnabled = false;
 
                     }
-                    if (credit == 0)
+                    if (data.credit == 0)
                     {
                         button2.IsEnabled = true;
                         button8.IsEnabled = false;
                         button5.IsEnabled = false;
 
                     }
+                    
+                    
+                    coutIntern++;
+                    data.counter += 1;
                 }
-
+                Data indiv = new Data
+                {
+                    counter = data.counter + 1,
+                    bet = -data.bet,
+                    credit = data.credit
+                };
+                result.Add(indiv);
+                string json = JsonConvert.SerializeObject(result, Formatting.Indented);
+                System.IO.File.AppendAllText(@"../../../TxtFile/Data.json", json);
                 button8.IsEnabled = true;
                 button5.IsEnabled = true;
+
+                //using (StreamReader r = new StreamReader(@"../../../TxtFile/Data.json"))
+                //{
+                //    string json = r.ReadToEnd();
+                //    result = JsonConvert.DeserializeObject<List<Data>>(json);
+                //}
             }
 
         }
         private void Button1_Click(object sender, RoutedEventArgs e)
         {
+            button = 1;
             button6.IsEnabled = false;
             button7.IsEnabled = false;
             button9.IsEnabled = false;
@@ -142,7 +200,7 @@ namespace CasinooWPF
 
         private void button2_Click(object sender, RoutedEventArgs e)
         {
-            credit = 50;
+            data.credit = 50;
             infolbl.Content = "CREDIT: " + "50 $";
 
             button3.IsEnabled = true;
@@ -154,51 +212,52 @@ namespace CasinooWPF
 
         private void button3_Click(object sender, RoutedEventArgs e)
         {
-            credit++;
-            infolbl.Content = "CREDIT: " + credit.ToString() + " $";
+            data.credit++;
+            infolbl.Content = "CREDIT: " + data.credit.ToString() + " $";
             button9.IsEnabled = true;
             button4.IsEnabled = true;
         }
 
         private void button4_Click(object sender, RoutedEventArgs e)
         {
-            if ((credit > 0) && (bet < credit)) credit--;
-            if (credit >= 0)
-                infolbl.Content = "CREDIT: " + credit.ToString() + " $";
-            if (credit == 0)
+            if ((data.credit > 0) && (data.bet < data.credit)) data.credit--;
+            if (data.credit >= 0)
+                infolbl.Content = "CREDIT: " + data.credit.ToString() + " $";
+            if (data.credit == 0)
             {
                 Button1.IsEnabled = false;
                 button9.IsEnabled = false;
             }
-            if (credit == 5) button4.IsEnabled = false;
+            if (data.credit == 5) button4.IsEnabled = false;
         }
 
         private void button5_Click(object sender, RoutedEventArgs e)
         {
-            if ((bet <= credit) && (bet > 0)) Button1.IsEnabled = true;
+            if ((data.bet <= data.credit) && (data.bet > 0)) Button1.IsEnabled = true;
             button6.IsEnabled = true;
             button7.IsEnabled = true;
             button5.IsEnabled = false;
-            betlbl.Content = "BET: " + bet.ToString() + " $";
+            Button10.IsEnabled = true;
+            betlbl.Content = "BET: " + data.bet.ToString() + " $";
         }
 
         private void button6_Click(object sender, RoutedEventArgs e)
         {
-            if (bet < credit)
+            if (data.bet < data.credit)
             {
                 Button1.IsEnabled = true;
-                bet++;
-                betlbl.Content = "BET: " + bet.ToString() + " $";
+                data.bet++;
+                betlbl.Content = "BET: " + data.bet.ToString() + " $";
             }
             button7.IsEnabled = true;
         }
 
         private void button7_Click(object sender, RoutedEventArgs e)
         {
-            if (bet > 0) bet--;
-            if (bet <= credit) Button1.IsEnabled = true;
-            betlbl.Content = "BET: " + bet.ToString() + " $";
-            if (bet == 0)
+            if (data.bet > 0) data.bet--;
+            if (data.bet <= data.credit) Button1.IsEnabled = true;
+            betlbl.Content = "BET: " + data.bet.ToString() + " $";
+            if (data.bet == 0)
             {
                 Button1.IsEnabled = false;
                 button7.IsEnabled = false;
@@ -218,8 +277,8 @@ namespace CasinooWPF
                 button9.IsEnabled = false;
                 button8.IsEnabled = false;
                 button2.IsEnabled = true;
-                credit = 50;
-                bet = 5;
+                data.credit = 50;
+                data.bet = 5;
                 infolbl.Content = "CREDIT: ";
                 betlbl.Content = "BET: ";
                 lblMsg.Content = "";
@@ -235,8 +294,18 @@ namespace CasinooWPF
             button9.IsEnabled = false;
             button5.IsEnabled = true;
 
-
         }
-        
+        private void Button10_Click(object sender, RoutedEventArgs e)
+        {
+            button = 2;
+            timer1.Interval = TimeSpan.FromMilliseconds(0);
+            timer1.Start();
+            //using (StreamReader r = new StreamReader(@"../../../TxtFile/Data.json"))
+            //{
+            //   string json = r.ReadToEnd();
+            //    result = JsonConvert.DeserializeObject<List<Data>>(json);
+            //}
+            //listBx.Items.Add(result);
+        }
     }
 }
